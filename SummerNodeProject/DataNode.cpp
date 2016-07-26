88f66eee2769a03894871a1099af2abf05f337ee
judyscheurm <cody.henrichsen@740s-pl-129.local>
2016-07-26 15:13:28 -0600

    getters and setters

--- SummerNodeProject/DataNode.cpp
+++ SummerNodeProject/DataNode.cpp
@@ -7,3 +7,44 @@
 //
 
 #include "DataNode.hpp"
+
+// Constructors
+
+template <class Type>
+DataNode<Type> :: DataNode()
+{
+    this->nodePointer = nullptr;
+}
+template <class Type>
+DataNode<Type> :: DataNode(Type nodeData)
+{
+    this ->nodeData = nodeData;
+    this->nodePointer = nullptr;
+}
+
+// End Constructors
+
+// Getters and Setters
+
+template <class Type>
+Type DataNode<Type> :: getNodeData()
+{
+    return nodeData;
+}
+
+template <class Type>
+DataNode<Type> * DataNode<Type> :: getNodePointer()
+{
+    return nodePointer;
+}
+template <class Type>
+void DataNode<Type> :: setNodeData (Type nodeData)
+{
+    this->nodeData = nodeData;
+}
+template <class Type>
+void DataNode<Type> :: setNodePointer(DataNode<Type>* nodePointer)
+{
+    this->nodePointer = nodePointer;
+}
+// End getters and setters
