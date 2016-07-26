4dc09976b998b54d8d5a8a78a88d0d06bf9c9586
judyscheurm <cody.henrichsen@740s-pl-129.local>
2016-07-26 15:30:18 -0600

    added class with tryNode and Start methods

---
+++ SummerNodeProject/NodeController.cpp
@@ -0,0 +1,18 @@
+//
+//  NodeController.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 Scheurman, Judy. All rights reserved.
+//
+
+#include "NodeController.hpp"
+void NodeController :: tryNodes()
+{
+    
+
+}
+void NodeController :: start()
+{
+    tryNodes();
+}
---
+++ SummerNodeProject/NodeController.hpp
@@ -0,0 +1,21 @@
+//
+//  NodeController.hpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 Scheurman, Judy. All rights reserved.
+//
+
+#ifndef NodeController_hpp
+#define NodeController_hpp
+
+#include <stdio.h>
+class NodeController
+{
+private:
+    void tryNodes();
+public:
+    void start();
+};
+
+#endif /* NodeController_hpp */
