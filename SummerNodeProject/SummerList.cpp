f0dba5de1ad1beeb3ebc8c1e03d1c008a84b6932
judyscheurm <cody.henrichsen@740s-pl-115.local>
2016-07-27 14:05:19 -0600

    creating cpp code in summer list

--- SummerNodeProject/SummerList.cpp
+++ SummerNodeProject/SummerList.cpp
@@ -7,3 +7,36 @@
 //
 
 #include "SummerList.hpp"
+#include <assert.h>
+
+template <class Type>
+SummerList<Type> :: SummerList()
+{
+    size = 0;
+    front = nullptr;
+    end = nullptr;
+}
+
+template <class Type>
+void SummerList<Type> :: addAtIndex(int index, Type data)
+{
+    assert (index >= 0 && index <= size);
+    // Different from arrat - we can include the end.
+    DataNode<Type> * indexPointer = front;
+    
+    //get to the spot before the spot I am adding to.
+    for(int position = 0; position < index - 1; position++)
+    {
+        indexPointer = indexPointer->getNodePointer();
+    }
+    
+    //Create a node.
+    DataNode<Type> * newNode = new DataNode<Type>(data);
+    
+    newNode->setNodePointer(indexPointer->getNodePointer());
+    //the line of code prior is the same as these 2 lines of commented
+    //DataNode<Type> *temp = indexPointer->getNodePointer();
+    //newNode->setNodePointer(temp);
+    
+}
+
