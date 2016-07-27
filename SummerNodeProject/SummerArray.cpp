c019a3fbfdfd8fe6136aaf35410b7bdbd3a06b97
judyscheurm <cody.henrichsen@740s-pl-115.local>
2016-07-27 10:00:46 -0600

    data members of summer array

---
+++ SummerNodeProject/SummerArray.cpp
@@ -0,0 +1,9 @@
+//
+//  SummerArray.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/27/16.
+//  Copyright © 2016 Scheurman, Judy. All rights reserved.
+//
+
+#include "SummerArray.hpp"
---
+++ SummerNodeProject/SummerArray.hpp
@@ -0,0 +1,37 @@
+//
+//  SummerArray.hpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/27/16.
+//  Copyright © 2016 Scheurman, Judy. All rights reserved.
+//
+
+#ifndef SummerArray_hpp
+#define SummerArray_hpp
+
+#include <stdio.h>
+#include "DataNode.hpp"
+
+
+template <class Type>
+class SummerArray
+{
+private:
+    int length;
+    bool * modifiedNodes;
+    DataNode<Type> front;
+    DataNode<Type> end;
+public:
+    SummerArray(int legth);
+    
+    Type getFormIndex(int index);
+    Type getFirst();
+    Type getLast();
+    void setAtIndex(int index, Type data);
+    
+    void sort();
+    void resize (int newSize);
+    int getUnusedNodes();
+    
+};
+#endif /* SummerArray_hpp */
