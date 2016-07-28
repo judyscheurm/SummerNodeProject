d32ba8a46d904e304f15c415b29045cffce6f979
judyscheurm <cody.henrichsen@740s-pl-115.local>
2016-07-28 15:50:12 -0600

    cout to try calculate method

--- SummerNodeProject/NodeController.cpp
+++ SummerNodeProject/NodeController.cpp
@@ -34,13 +34,18 @@ void NodeController :: tryArray()
 void NodeController :: tryTree()
 {
     BinaryTree<int> sampleTree;
+    cout << "The tree is this big: " << sampleTree.getSize() << endl;
     sampleTree.insert(7);
     sampleTree.insert(5);
+    cout << "The tree is this big: " << sampleTree.getSize() << endl;
     sampleTree.insert(213);
+    sampleTree.insert(-123);
+    cout << "The tree is this big: " << sampleTree.getSize() << endl;
     sampleTree.insert(5);
     sampleTree.insert(123124);
     sampleTree.insert(0);
-    sampleTree.insert(-123);
+    cout << "The tree is this big: " << sampleTree.getSize() << endl;
+    
     
     cout << "The in order traversal" << endl;
     sampleTree.inOrderTraversal(sampleTree.getRoot());
