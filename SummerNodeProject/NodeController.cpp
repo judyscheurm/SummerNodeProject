43f1d50f8ea2f796613807be2c192510bc579f59
judyscheurm <cody.henrichsen@740s-pl-115.local>
2016-07-28 14:45:01 -0600

    traversal methods corrections

--- SummerNodeProject/NodeController.cpp
+++ SummerNodeProject/NodeController.cpp
@@ -43,6 +43,15 @@ void NodeController :: tryTree()
     
     cout << "The in order traversal" << endl;
     sampleTree.inOrderTraversal(sampleTree.getRoot());
+    cout << endl;
+    
+    cout << "The pre order traversal" << endl;
+    sampleTree.preOrderTraversal(sampleTree.getRoot());
+    cout << endl;
+    
+    cout << "The post order traversal" << endl;
+    sampleTree.postOrderTraversal(sampleTree.getRoot());
+    cout << endl;
     
 }
 
